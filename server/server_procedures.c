6894f64452d4cd43b41685a9aa28feec9815035c
Vitya535 <kushnerenko.viktor@gmail.com>
2022-04-24 13:42:07 +0300

    Add server side functionality

--- server/server_procedures.c
+++ server/server_procedures.c
@@ -1,57 +1,88 @@
 #undef UNICODE
 
 #include <stdio.h>
+#include <stdbool.h>
 #include "../com/chat_rpc.h"
 
-void HelloProc(unsigned char *pszString)
-{
-	printf("%s\n", pszString);
-}
+struct users {
+	unsigned char *userName;
+	int clientId;
+	bool isOnline;
+} userList[USERS_COUNT];
 
-void Shutdown(void)
-{
-	RPC_STATUS status;
+struct messages {
+	int messageId;
+	int senderId;
+	unsigned char *text;
+	unsigned char *receiverName;
+	bool isMessageReceived;
+} messageList[MESSAGES_COUNT];
 
-	status = RpcMgmtStopServerListening(NULL);
+static int userListIndex = -1;
+static int messageListIndex = -1;
 
-	printf("RpcMgmtStopServerListening returned 0x%x\n", status);
-
-	if (status)
-		exit(status);
-
-	/*status = RPCServerUnregisterIf(NULL, NULL, FALSE);
-
-	printf("RPCServerUnregisterIf returned 0x%x\n", status);
-
-	if (status)
-		exit(status);*/
-}
+// ToDo - поправить глобально кодировку записи символов
 
 int Login(unsigned char *userName)
 {
+	if (strlen(userName) <= STR_LEN) {
+		userListIndex++;
+		userList[userListIndex].userName = userName;
+		userList[userListIndex].clientId = userListIndex; // FYI: clientId совпадет с индексом
+		userList[userListIndex].isOnline = true;
+		return userList[userListIndex].clientId;
+	}
 	return 0;
+	// ToDo - Валидация если больше 50 символов на стороне клиента?
 }
 
-void Logout(int id)
+void Logout(int clientId)
 {
+	userList[clientId].isOnline = false;
 }
 
-int GetUsersList(unsigned char usersList[100][50])
+int GetUsersList(unsigned char *usersList[USERS_COUNT])
 {
-	return 0;
+	for (int i = 0; i < USERS_COUNT; i++) {
+		usersList[i] = userList[i].userName;
+	}
+	return 0; // ToDo - может void раз мы список заполняем?
 }
 
-int SendMyMessage(int id, unsigned char *message, unsigned char *receiver)
+int SendMyMessage(int clientId, unsigned char *message, unsigned char *receiver)
 {
-	return 0;
+	if (strlen(message) <= STR_LEN) {
+		messageListIndex++;
+		messageList[messageListIndex].messageId = messageListIndex; // FYI: messageId совпадет с индексом
+		messageList[messageListIndex].senderId = clientId;
+		messageList[messageListIndex].text = message;
+		messageList[messageListIndex].receiverName = receiver;
+		messageList[messageListIndex].isMessageReceived = false;
+		return messageList[messageListIndex].messageId;
+	}
+	return -1;
+	// ToDo - валидация на длину сообщения и несуществующего получателя на стороне клиента?
 }
 
-int GetMessageStatus(int id, int messageId)
+int GetMessageStatus(int clientId, int messageId) // похоже clientId тут и не нужен: нам есть разница какой пользователь его должен получить? (помоему важен только messageId)
 {
-	return 0;
+	return messageList[messageId].isMessageReceived;
+	// ToDo - статус на клиенте отображается в виде цифры (может текстом надо?)
 }
 
-int ReceiveMyMessage(unsigned char usersList[100], unsigned char sender[100])
+int ReceiveMyMessage(int clientId, unsigned char *sender, unsigned char *message)
 {
-	return 0;
+	unsigned char *clientUserName = userList[clientId].userName;
+	for (int i = 0; i < MESSAGES_COUNT; i++) {
+		if (messageList[i].receiverName == clientUserName) {
+			for (int j = 0; j < USERS_COUNT; j++) {
+				if (userList[j].clientId == messageList[i].senderId) {
+					sender = userList[j].userName;
+					break;
+				}
+			}
+			message = messageList[i].text;
+		}
+	}
+	return 0; // ToDo - может void раз мы переменные, подающиеся на вход, заполняем? 
 }
